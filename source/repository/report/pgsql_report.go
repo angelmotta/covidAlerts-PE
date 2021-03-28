dfb5fc386c4e6d95c0fe752ee8a1ce48feb6e797
Angel Motta <angelmotta@gmail.com>
2021-03-28 08:15:12 -0500

    Repository interface done

--- source/repository/report/pgsql_report.go
+++ source/repository/report/pgsql_report.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"github.com/angelmotta/covidAlerts-PE/source/model"
 	"github.com/angelmotta/covidAlerts-PE/source/repository"
+	"log"
 )
 
 // pgsqlReportRepository will implement the interface 'repository.Repository'
@@ -16,9 +17,27 @@ func NewPgSqlReportRepository (Conn *sql.DB) repository.Repository {
 	return &pgsqlReportRepository{Conn}
 }
 
-// pgsqlReportRepository implements 'pgsqlReportRepository' interface
+// pgsqlReportRepository implements 'pgsqlReportRepository' Interface
 func (pgRepo *pgsqlReportRepository) Create(report *model.CasesReport) error {
-	// TODO
-	//sqlStatement := `INSERT INTO dailycases (date, newcases) VALUES ($1, $2)`
+	// Prepare statement
+	stmt, err := pgRepo.Conn.Prepare("INSERT INTO dailycases (date, newcases) VALUES ($1, $2)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	// Execute Sql statement
+	res, err := stmt.Exec(report.Date, report.NewCases)	// 'Date' string, 'NewCases' int
+	if err != nil {
+		return err
+	}
+
+	// Validate executed statement
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	log.Printf("DBMetada: #Rows affected = %d\n", rowCnt)
+
 	return nil
 }
